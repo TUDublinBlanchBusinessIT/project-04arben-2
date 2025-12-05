ba3049fc3ba6aa8e6924caca4cec0638c5d67d1f
04arben <b00168185@mytudublin.ie>
2025-12-05 00:00:02 +0000

    Made so that login directs to a new page

--- login_process.php
+++ login_process.php
@@ -12,18 +12,23 @@ $pass = $_POST['password'];
 include("db_connection.php");
 
 
+
+
+
 $sql = "INSERT INTO users (username, password) VALUES ('$user', '$pass')";
-
-
-
 
 if (mysqli_query($conn, $sql)) {
 
     $_SESSION['user'] = $user;
 
-    echo "<br>New user created successfully! You are now logged in as " . $user;
+
+
+    header("Location: select_movie.php");
+    exit(); 
 
 } else {
+
+
 
     echo "<br>Error: " . $sql . "<br>" . mysqli_error($conn);
 
---
+++ select_movie.php
@@ -0,0 +1,12 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Select Movie</title>
+</head>
+<body>
+    <h1>Select Movie</h1>
+</body>
+</html>
