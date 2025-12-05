3e996ef6a32b433c68cab5836b241545fa6d29ae
04arben <b00168185@mytudublin.ie>
2025-12-05 23:18:39 +0000

    I added page logic and wrote a html code below it, later I'm going to be adding a dropdown menu into select_movie.php where user will be choosing a movie from 'movies' table within the database

--- select_movie.php
+++ select_movie.php
@@ -1,12 +1,26 @@
 <?php
 session_start();
+
+if (!isset($_SESSION['user'])) {
+    header("Location: login.html");
+    exit();
+
+}
+
 ?>
+
+
+
+
+
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
+    <meta charset="UTF-8">
     <title>Select Movie</title>
 </head>
 <body>
-    <h1>Select Movie</h1>
+    <h1>Welcome to Movie Selection</h1>
+    <p>Please select a movie from the list below.</p>
 </body>
 </html>
