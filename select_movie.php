c5ae5884729675f128603ac1838f144e3fb1ae0b
04arben <b00168185@mytudublin.ie>
2025-12-06 21:29:46 +0000

    Added a working Bootstrap Dropdown Menu into the movie selection page

--- select_movie.php
+++ select_movie.php
@@ -4,23 +4,46 @@ session_start();
 if (!isset($_SESSION['user'])) {
     header("Location: login.html");
     exit();
-
 }
-
 ?>
-
-
-
-
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title>Select Movie</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
+
 </head>
 <body>
-    <h1>Welcome to Movie Selection</h1>
-    <p>Please select a movie from the list below.</p>
+
+
+<br><br><br>
+<h1>Select a Movie</h1>
+
+
+
+
+<div class="dropdown">
+  <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown">
+    Select a Movie
+  </button>
+
+
+
+
+  <div class="dropdown-menu">
+      <a class="dropdown-item" href="#">Movie 1</a>
+      <a class="dropdown-item" href="#">Movie 2</a>
+      <a class="dropdown-item" href="#">Movie 3</a>
+      <a class="dropdown-item" href="#">Movie 4</a>
+  </div>
+</div>
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
